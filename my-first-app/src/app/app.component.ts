1e7f594a03ce1509a0e4105d685346a259a5a534
spyridon <spyridon@yobetit.com>
2018-07-31 10:27:01 +0200

    Another option for ngFor with Date

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+
+
   }
 }
