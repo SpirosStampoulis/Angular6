f3e4ee042e2d32918ebc04fac050e676519ad61e
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2018-07-31 08:20:07 +0200

    ngIf nsStyle ngIf ngFor Directives

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -10,8 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = 'No server created';
+  serverCreationStatus = 'No server was created';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserever 2'];
 
   constructor() {
     setTimeout(() => {
@@ -23,6 +25,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
   }
 
