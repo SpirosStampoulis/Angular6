1c2d48a7728da47010308fa5f1cf0a9734d9e375
spyridon <spyridon@yobetit.com>
2018-08-01 12:50:01 +0200

    small fix

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recioe';
+  loadedFeature = 'recipe';
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
