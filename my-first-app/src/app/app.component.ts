8ed9940237295e0717b4117c9833ed18f88945ce
spyridon <spyridon@yobetit.com>
2018-08-02 09:50:03 +0200

    create 2 components account and new-account and create 2 services account and logging

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-app';
+export class AppComponent implements OnInit {
+  accounts: {name: string, status: string}[] = [];
+
+  constructor(private accountsService: AccountsService) {}
+
+  ngOnInit() {
+    this.accounts = this.accountsService.accounts;
+  }
 }
