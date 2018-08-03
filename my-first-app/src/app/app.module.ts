ec884dada832aa2e36d27480da36e6332514d6e5
spyridon <spyridon@yobetit.com>
2018-08-03 12:06:23 +0200

    129 nested routing change the ap-component with router outlet

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -13,14 +13,17 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
+
+// create routes with children
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'users', component: UsersComponent},
-  { path: 'users/:id/:name', component: UserComponent},
-  { path: 'servers', component: ServersComponent},
-  { path: 'servers/:id', component: ServerComponent},
-  { path: 'servers/:id/edit', component: EditServerComponent}
-
+  { path: 'users', component: UsersComponent, children: [
+    { path: ':id/:name', component: UserComponent}
+  ]},
+  { path: 'servers', component: ServersComponent, children: [
+  { path: ':id', component: ServerComponent},
+  { path: ':id/edit', component: EditServerComponent}
+  ] }
 ];
 @NgModule({
   declarations: [
