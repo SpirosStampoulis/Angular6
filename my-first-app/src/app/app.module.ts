f55295dfdcd77cde57cfd02f2f2d8f7bf27e6a1c
spyridon <spyridon@yobetit.com>
2018-08-03 13:30:28 +0200

    redirect ot page not found when the link is oncorrect

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -12,9 +12,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 // create routes with children
+// ** catch all paths and this one should be the last one
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent, children: [
@@ -23,7 +25,9 @@ const appRoutes: Routes = [
   { path: 'servers', component: ServersComponent, children: [
   { path: ':id', component: ServerComponent},
   { path: ':id/edit', component: EditServerComponent}
-  ] }
+  ] },
+  { path: 'not-found', component: PageNotFoundComponent},
+  { path: '**', redirectTo: '/not-found'}
 ];
 @NgModule({
   declarations: [
@@ -33,7 +37,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
