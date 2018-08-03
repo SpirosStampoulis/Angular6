a1a8be85187be45436498512523f7e939ccc5a9f
spyridon <spyridon@yobetit.com>
2018-08-03 09:53:03 +0200

    123 create url dynamic with id and name

--- my-first-app/src/app/users/user/user.component.ts
+++ my-first-app/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  // inject the active route
+  constructor(public route: ActivatedRoute) { }
 
+  // when the component get initialized
   ngOnInit() {
-  }
-
+    this.user = {
+    id: this.route.snapshot.params['id'],
+    name: this.route.snapshot.params['name']
+  };
+ }
 }
