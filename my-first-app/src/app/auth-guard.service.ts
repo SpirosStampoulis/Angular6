b285e6c06f9aeeb2e84a363138b09beea8adb022
spyridon <spyridon@yobetit.com>
2018-08-03 16:56:33 +0200

    spelling fix

--- my-first-app/src/app/auth-guard.service.ts
+++ my-first-app/src/app/auth-guard.service.ts
@@ -1,36 +1,35 @@
 import {
-     CanActivate,
-     ActivatedRouteSnapshot,
-     RouterStateSnapshot,
-     Router,
-     CanActivateChild
-} from '@angular/router';
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    Router,
+    CanActivateChild
+  } from '@angular/router';
+  import { Observable } from 'rxjs/Observable';
+  import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
-import { Injectable } from '@angular/core';
+  import { AuthService } from './auth.service';
 
-import { AuthService } from './auth.service';
-
-
-@Injectable()
-export class AuthGuard implements CanActivate, CanActivateChild {
-    constractor(private authService: AuthService, private router: Router) { }
+  @Injectable()
+  export class AuthGuard implements CanActivate, CanActivateChild {
+    constructor(private authService: AuthService, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-        return this.authService.isAuthenticated()
-            .then(
-                (authenticated: boolean) => {
-                 if (authenticated) {
-                    return true;
-                } else {
-                   this.router.navigate(['/']);
-                }
+                state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      return this.authService.isAuthenticated()
+        .then(
+          (authenticated: boolean) => {
+            if (authenticated) {
+              return true;
+            } else {
+              this.router.navigate(['/']);
             }
-         );
+          }
+        );
     }
+
     canActivateChild(route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-        return this.canActivate(route, state);
+                     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      return this.canActivate(route, state);
     }
-}
+  }
