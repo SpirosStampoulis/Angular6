b05d378fbc361d1b7d84e7bf044de0fc1fb19633
spyridon <spyridon@yobetit.com>
2018-08-06 12:30:06 +0200

    141 Using resolve guard for Dynamic Data

--- my-first-app/src/app/app-routing.module.ts
+++ my-first-app/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 
 // create routes with children
@@ -24,7 +25,7 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
-    { path: ':id', component: ServerComponent},
+    { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
     ] },
     // { path: 'not-found', component: PageNotFoundComponent},
--- my-first-app/src/app/servers/server/server.component.ts
+++ my-first-app/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -15,13 +15,19 @@ export class ServerComponent implements OnInit {
   public router: Router) { }
 //  + to retrive as number
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(1);
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(+params['id']);
+    this.route.data
+    .subscribe(
+      (data: Data) => {
+        this.server = data['server'];
       }
     );
+    // const id = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(1);
+    // this.route.params.subscribe(
+    //   (params: Params) => {
+    //     this.server = this.serversService.getServer(+params['id']);
+    //   }
+    // );
 
   }
   onEdit() {
