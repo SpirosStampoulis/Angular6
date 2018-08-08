aca69e5053fb3ca639b58d82b2c1f5c6b550d5bd
spyridon <spyridon@yobetit.com>
2018-08-08 15:04:08 +0200

    164 Unsubscribe observables to free memory

--- my-first-app/src/app/home/home.component.ts
+++ my-first-app/src/app/home/home.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscribable } from 'rxjs/Observable';
 // to add interval
 // tslint:disable-next-line:import-blacklist
 import 'rxjs/Rx';
 import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  // stored in one property
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    // const Mynumbers = Observable.interval(1000);
-    // Mynumbers.subscribe(
-    //   (number: number) => {
-    //     console.log(number);
-    //   }
-    // );
+    const Mynumbers = Observable.interval(1000);
+    this.numbersObsSubscription = Mynumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
     // to create a btodge between observable and observer
     // next push the next data packages
     // Observer need to define what data
@@ -41,12 +45,19 @@ export class HomeComponent implements OnInit {
         observer.next('third package');
       }, 7000);
     });
+    // save my observables
     // and now subscribe them
-    myObservable.subscribe(
+    this.customObsSubscription = myObservable.subscribe(
       (data: string) => { console.log(data); },
       (error: string) => { console.log(error); },
       () => { console.log('completed'); }
     );
+  }
+
+  // to free memory and stop calling Observables when navigate
+  ngOnDestroy() {
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
 
 }
