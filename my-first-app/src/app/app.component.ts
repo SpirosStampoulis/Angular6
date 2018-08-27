4c3aefd29855663801821239ad1bf4ac7d34fd33
spyridon <spyridon@yobetit.com>
2018-08-27 15:18:22 +0200

    create an array with forbidden usernames for the input of username

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,16 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
+
+  constructor(private forBuilder: FormBuilder) {}
 
   ngOnInit() {
     // here will confugure the form and we ll put the validators and dont use () we dont want to executed we want just the reference
     this.signupForm = new FormGroup({
       // pass the object in the form group and now username and email are nested
       'userData': new FormGroup({
-       'username': new FormControl(null, Validators.required),
+      'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -30,5 +34,12 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // check if thename exist in forbidden names
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
   }
 }
